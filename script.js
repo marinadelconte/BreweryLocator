450dfcf1f1064e3444eed9e5736dc2da8a417bc7
Marina Del Conte <marinadelconte@yahoo.com>
2023-08-09 10:09:08 -0400

    committing to merge

--- script.js
+++ script.js
@@ -86,53 +86,53 @@ searchButton.addEventListener("click", function (event){
 
 
 
-//Waiting for buttons to be created
+Waiting for buttons to be created
 
-// let requestJoke = document.querySelector("#jokeBtn");
-// let returnJoke = document.querySelector("#joke")
-// let anotherJoke = document.querySelector("#badJoke");
-// let keeper = document.querySelector("#goodJoke");
+let requestJoke = document.querySelector("#jokeBtn");
+let returnJoke = document.querySelector("#joke")
+let anotherJoke = document.querySelector("#badJoke");
+let keeper = document.querySelector("#goodJoke");
 
 
 
-// requestJoke.addEventListener("click", function (event) {
-// event.target.matches("#jokeBtn");
+requestJoke.addEventListener("click", function (event) {
+event.target.matches("#jokeBtn");
 
-// fetch('https://geek-jokes.sameerkumar.website/api?format=json')
-// .then(function(response){
+fetch('https://geek-jokes.sameerkumar.website/api?format=json')
+.then(function(response){
     
-// return response.json()     
-// })
-// .then(function (newJoke) {
-//     console.log(newJoke);
-//     let randomJoke = newJoke.joke; 
-//     returnJoke.textContent = randomJoke;
-// })
-// })
+return response.json()     
+})
+.then(function (newJoke) {
+    console.log(newJoke);
+    let randomJoke = newJoke.joke; 
+    returnJoke.textContent = randomJoke;
+})
+})
 
-// anotherJoke.addEventListener("click", function(event) {
+anotherJoke.addEventListener("click", function(event) {
 
 
-//     event.target.matches("#badJoke");
+    event.target.matches("#badJoke");
 
    
-//     fetch('https://geek-jokes.sameerkumar.website/api?format=json')
-//     .then(function(response){
+    fetch('https://geek-jokes.sameerkumar.website/api?format=json')
+    .then(function(response){
         
-//     return response.json()     
-//     })
-//     .then(function (newJoke) {
-//         console.log(newJoke);
-//         randomJoke = newJoke.joke; 
-//         returnJoke.textContent = randomJoke;
-//     })
-// })
+    return response.json()     
+    })
+    .then(function (newJoke) {
+        console.log(newJoke);
+        randomJoke = newJoke.joke; 
+        returnJoke.textContent = randomJoke;
+    })
+})
 
-// keeper.addEventListener("click", function(event) {
+keeper.addEventListener("click", function(event) {
 
-//     event.target.matches("goodJoke");
+    event.target.matches("goodJoke");
 
-//     localStorage.setItem("randomJoke", returnJoke.textContent);
-// })
+    localStorage.setItem("randomJoke", returnJoke.textContent);
+})
 
 
